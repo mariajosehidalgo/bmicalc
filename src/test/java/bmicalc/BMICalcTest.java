10ebbf9b4c6127b0c02759d2bddb325364e0c739
mariajosehidalgo <mariajose.hidalgo@uma.es>
2023-02-20 21:19:11 +0100

    TEST DEFINIDOS

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -12,4 +12,58 @@ public class BMICalcTest {
 	public void bmi() {
 		assertEquals(true, true);
 	}
+	
+	@Test
+	@DisplayName("TEST CALCULO BMI")
+	public void test1() {
+		
+	}
+	
+	@Test
+	@DisplayName("BMI<18.5")
+	public void test2() {
+		
+	}
+	
+	@Test
+	@DisplayName("BMI=18.5")
+	public void test3() {
+		
+	}
+	
+	@Test
+	@DisplayName("18.5<BMI<=24.9")
+	public void test4() {
+		
+	}
+	
+	@Test
+	@DisplayName("BMI=25.0")
+	public void test5() {
+		
+	}
+	
+	@Test
+	@DisplayName("25.0<BMI<=29.9")
+	public void test6() {
+		
+	}
+	
+	@Test
+	@DisplayName("BMI=30.0")
+	public void test7() {
+		
+	}
+	
+	@Test
+	@DisplayName("BMI>30")
+	public void test8() {
+		
+	}
+	
+	@Test
+	@DisplayName("TEST CALCULO ABDOMINALOBESITY")
+	public void test9() {
+		
+	}
 }
