2d28628483e9606c4c3e4eb663cd41f7a632117e
mariajosehidalgo <mariajose.hidalgo@uma.es>
2023-02-20 21:25:27 +0100

    IMPLEMENTACION DEL METODO BMI Y TEST PARA ESTE METODO IMPLEMENTADOS

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,7 +3,10 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return 0.0;
+		if(mass<=0 || height<=0) {
+			throw new RuntimeException("La masa y la altura deben ser valores positivos");
+		}
+		return Math.round(mass/Math.pow(height, 2)*10)/10;
 	}
 
 	public String category(double bmi) {
--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,11 +1,19 @@
 package bmicalc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
+	
+private BMICalcImpl bmi;
+	
+	@BeforeEach
+	void init() {
+		bmi = new BMICalcImpl();
+	}
 	
 	@Test
 	@DisplayName("Example Test Method.")
@@ -16,7 +24,12 @@ public class BMICalcTest {
 	@Test
 	@DisplayName("TEST CALCULO BMI")
 	public void test1() {
-		
+		assertEquals(23.62, bmi.bmi(62, 1.62));
+		assertEquals(19.36, bmi.bmi(54, 1.67));
+		assertEquals(27.77, bmi.bmi(40, 1.20));
+		assertEquals(31.21, bmi.bmi(12, 0.62));
+		assertEquals(26.87, bmi.bmi(90, 1.83));
+		assertEquals(25.09, bmi.bmi(70, 1.67));
 	}
 	
 	@Test
