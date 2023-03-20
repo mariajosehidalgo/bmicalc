e21566ddce72d14155b4c7265d0ad7a26350b831
mariajosehidalgo <mariajose.hidalgo@uma.es>
2023-03-20 11:31:29 +0100

    STEPDEFINITIONS MODIFICADO Y CLASE PARA TRATAR ERROR NEGATIVO CREADA

---
+++ src/main/java/bmicalc/NegativeValueException.java
@@ -0,0 +1,19 @@
+package bmicalc;
+
+/**
+ * @author Antonio J. Nebro
+ */
+public class NegativeValueException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+/**
+   * Exception thrown when the value of the parameter of the {@link Factorial#compute} method is a
+   * negative number
+   *
+   * @param errorMessage
+   */
+  public NegativeValueException(String errorMessage) {
+    super(errorMessage);
+  }
+}
--- src/test/java/bmicalc/StepDefinitions.java
+++ src/test/java/bmicalc/StepDefinitions.java
@@ -1,5 +1,50 @@
 package bmicalc;
 
+import org.junit.jupiter.api.Assertions;
+import bmicalc.NegativeValueException;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+
 public class StepDefinitions {
 
+	private BMICalcImpl bmi;
+	private double result ;
+	private boolean raiseException;
+	
+	@Before
+	public void init() {
+		bmi = null;
+		result = (Double) null;
+		raiseException = false;
+	}
+	
+	@Given("Tenemos inicializada la calculadora")
+	public void tenemos_inicializada_la_calculadora() {
+	    bmi = new BMICalcImpl();
+	}
+
+	@When("El usuario introduce valores validos de {double} y {double}")
+	public void el_usuario_introduce_valores_validos_de_y(Double double1, Double double2) throws Exception {
+		result = bmi.bmi(double1, double2);
+	}
+
+	@Then("La calculadora devuelve el valor del {double} del usuario")
+	public void la_calculadora_devuelve_el_valor_del_del_usuario(Double double1) {
+		Assertions.assertEquals(double1, result);
+	}
+
+	@When("El usuario introduce valores invalidos de {double} y {double}")
+	public void el_usuario_introduce_valores_invalidos_de_y(Double double1, Double double2) throws Exception {
+		try {
+			result = bmi.bmi(double1, double2);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+
+	@Then("La calculadora lanza un error en el que informa que los datos no estan en un formato valido. Vuelve al menu original.")
+	public void la_calculadora_lanza_un_error_en_el_que_informa_que_los_datos_no_estan_en_un_formato_valido_vuelve_al_menu_original() {
+		Assertions.assertTrue(raiseException);
+	}
+	
 }
